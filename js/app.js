16c1302edbd5342d3351dbf5649a97c3cd33c948
faysal <faysal.mehmood101@gmail.com>
2021-11-16 23:15:13 +0500

    add media query to header

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-  $(".food-slider").slick({
+  $('.food-slider').slick({
     autoplay: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: ".prev-btn",
-    nextArrow: ".next-btn",
+    prevArrow: '.prev-btn',
+    nextArrow: '.next-btn',
   });
+});
+$('.nav-trigger').click(function () {
+  $('.site-content-wrapper').toggleClass('scaled');
 });
